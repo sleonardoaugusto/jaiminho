32a930ed2e8ba494e57df35b2a6090911c7763c3
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-07-20 22:02:27 -0300

    Functionality moved to er_customs app

--- jaiminho/hooks.py
+++ jaiminho/hooks.py
@@ -26,7 +26,6 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Address": "public/js/address.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
